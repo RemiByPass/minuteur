6930fbd1372f12269327003742d559a5889d863f
RemiByPass <49651279+RemiByPass@users.noreply.github.com>
2020-10-15 18:58:42 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import time
+print("Codez par Remi_ByPass#7867")
+print("Ajoutez un temps de décompte (en seconde)")
+decompte = float(input("Nombres de secondes = "))
+time.sleep(1)
+print("Un décompte de", decompte, "secondes va ce lancer...")
+print("Décompte lancer...")
+time.sleep(0.5)
+while decompte >= 0:
+    print(decompte, "secondes restantes...")
+    decompte = decompte - 1
+    time.sleep(1)
+print("Terminer !")
